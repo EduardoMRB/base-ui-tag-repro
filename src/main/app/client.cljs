c9a4523e0811537b69c974134920f1d8df9eb260
Eduardo <eduardomrb@gmail.com>
2026-06-19 17:25:54 -0300

    Tidy up

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -1,18 +1,17 @@
 (ns app.client
   (:require
-    [app.application :refer [SPA]]
-    [app.model.session :as session]
-    [app.ui.root :as root]
-    [app.ui.root :as root]
-    [com.fulcrologic.fulcro-css.css-injection :as cssi]
-    [com.fulcrologic.fulcro.algorithms.denormalize :as fdn]
-    [com.fulcrologic.fulcro.algorithms.merge :as merge]
-    [com.fulcrologic.fulcro.application :as app]
-    [com.fulcrologic.fulcro.components :as comp]
-    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
-    [com.fulcrologic.fulcro.ui-state-machines :as uism]
-    [fulcro.inspect.tool :as it]
-    [taoensso.timbre :as log]))
+   [app.application :refer [SPA]]
+   [app.model.session :as session]
+   [app.ui.root :as root]
+   [com.fulcrologic.fulcro-css.css-injection :as cssi]
+   [com.fulcrologic.fulcro.algorithms.denormalize :as fdn]
+   [com.fulcrologic.fulcro.algorithms.merge :as merge]
+   [com.fulcrologic.fulcro.application :as app]
+   [com.fulcrologic.fulcro.components :as comp]
+   [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
+   [com.fulcrologic.fulcro.ui-state-machines :as uism]
+   [fulcro.inspect.tool :as it]
+   [taoensso.timbre :as log]))
 
 (defn ^:export refresh []
   (log/info "Hot code Remount")
@@ -23,13 +22,9 @@
   (log/info "Application starting.")
   (cssi/upsert-css "componentcss" {:component root/Root})
   (app/set-root! SPA root/Root {:initialize-state? true})
-  ;(inspect/app-started! SPA)
   (dr/initialize! SPA)
   (it/add-fulcro-inspect! SPA)
   (log/info "Starting session machine.")
-  (uism/begin! SPA session/session-machine ::session/session
-    {:actor/login-form      root/Login
-     :actor/current-session root/Session})
   (app/mount! SPA root/Root "app" {:initialize-state? false}))
 
 (comment
